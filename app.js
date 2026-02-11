df30dde2db2e4e5c260ee141a1db39d3652a5f4a
JOHNATHAN VIRGIL <JOHNATHANVIRGIL@YMAIL.COM>
2026-02-11 09:04:24 -0800

    final revison

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const { PORT = 3001, MONGODB_URI } = process.env;
+const { PORT = 3001, MONGODB_URI = "mongodb://127.0.0.1:27017/wtwr_db" } = process.env;
 
 const mainRouter = require("./routes/index");
 
 const app = express();
 
 app.use(express.json()); // ✅ put BEFORE routes
+app.use((req, res, next) => {
+  req.user = {
+    _id: "698ca7c56ad3550b28824314"
+  };
+  next();
+});
 app.use("/", mainRouter);
 
 mongoose
